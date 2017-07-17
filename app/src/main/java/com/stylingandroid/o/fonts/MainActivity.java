25743198135b3556e8697f7e7ee111cf4da4d0dd
Prashant Chovatiya <prashant.chovatiya@eliteinfoworld.com>
2017-07-17 14:25:15 +0530

    First commit

--- app/src/main/java/com/stylingandroid/o/fonts/MainActivity.java
+++ app/src/main/java/com/stylingandroid/o/fonts/MainActivity.java
@@ -1,19 +1,22 @@
 package com.stylingandroid.o.fonts;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    @TargetApi(Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Typeface nunito = getResources().getFont(R.font.nunito);
+       /* Typeface nunito = getResources().getFont(R.font.nunito);
         TextView text = (TextView) findViewById(R.id.nunito_programmatic);
-        text.setTypeface(nunito, Typeface.BOLD_ITALIC);
+        text.setTypeface(nunito, Typeface.BOLD_ITALIC);*/
     }
 }
